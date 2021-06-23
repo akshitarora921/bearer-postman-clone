8805649311ef1be9337c8919eeebdd505bd13bb7
Akshit Arora <akshitarora9211@gmail.com>
2021-06-23 14:12:38 +0530

    final comment

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import prettyBytes from "pretty-bytes";
+import setupEditor from "./setup-editor";
 
+const { requestEditor, updateResponseEditor } = setupEditor();
 const form = document.querySelector("[data-form]");
 const queryParamsContainer = document.querySelector("[data-query-params]");
 const requestHeadersContainer = document.querySelector(
@@ -10,6 +13,10 @@ const requestHeadersContainer = document.querySelector(
 const keyValuePairTemplate = document.querySelector(
   "[data-key-value-template]"
 );
+const responseHeadersContainer = document.querySelector(
+  "[data-response-headers]"
+);
+const responseBodyContainer = document.querySelector("[data-response-body]");
 
 document
   .querySelector("[data-add-query-params-btn]")
@@ -24,14 +31,49 @@ document
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
+axios.interceptors.request.use((request) => {
+  request.customData = request.customData || {};
+  request.customData.startTime = new Date().getTime();
+  return request;
+});
+
+function updateEndTime(response) {
+  response.customData = response.customData || {};
+  response.customData.time =
+    new Date().getTime() - response.config.customData.startTime;
+  return response;
+}
+axios.interceptors.response.use(updateEndTime, (e) => {
+  return Promise.reject(updateEndTime(e.response));
+});
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  let data;
+  try {
+    data = JSON.parse(requestEditor.state.doc.toString() || null);
+  } catch (e) {
+    alert("JSON data is malformed!");
+    return;
+  }
+
   axios({
     url: document.querySelector("[data-url]").value,
     method: document.querySelector("[data-method]").value,
     params: keyValuePairToObject(queryParamsContainer),
     headers: keyValuePairToObject(requestHeadersContainer),
-  }).then((data) => console.log(data));
+    data,
+  })
+    .catch((err) => err)
+    .then((response) => {
+      document
+        .querySelector("[data-response-section]")
+        .classList.remove("d-none");
+      updateResponseDetails(response);
+      updateResponseEditor(response.data);
+      updateResponseHeaders(response.headers);
+    });
 });
 
 function createKeyValuePair() {
@@ -50,4 +92,27 @@ function keyValuePairToObject(container) {
     if (key === "") return data;
     return { ...data, [key]: value };
   }, {});
+}
+
+function updateResponseDetails(response) {
+  document.querySelector("[data-status]").textContent = response.status;
+  document.querySelector("[data-time]").textContent = response.customData.time;
+  document.querySelector("[data-size]").textContent = prettyBytes(
+    JSON.stringify(response.data).length +
+      JSON.stringify(response.headers).length
+  );
+}
+
+function updateResponseHeaders(headers) {
+  responseHeadersContainer.innerHTML = "";
+  console.log(responseHeadersContainer);
+  Object.entries(headers).forEach(([key, value]) => {
+    console.log(key, value);
+    const keyElement = document.createElement("div");
+    keyElement.textContent = key;
+    responseHeadersContainer.append(keyElement);
+    const valueElement = document.createElement("div");
+    valueElement.textContent = value;
+    responseHeadersContainer.append(valueElement);
+  });
 }
